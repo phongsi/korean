9eb7259395046a763443aab4614875d387008684
Phong Si <phong.si@gmail.com>
2011-05-08 22:59:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Korean::Application.config.secret_token = '02dfabaf0f353f804fd33dfa9ee7eb42da56e4ac316e64336a483b1e700d99652a29b65dc8b6aa414fe29c38eb56a258a8cbe556dbcc560ff46c0443baabea97'
